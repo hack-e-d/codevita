7caed4257c5c9f0c37586ae25e8fc4fc9686e451
Vijay P V <52239002+hack-e-d@users.noreply.github.com>
2019-06-29 13:47:38 +0530

    Add files via upload

--- Harmonieno.py
+++ Harmonieno.py
@@ -1,11 +1,28 @@
 def fact(n):
     if(n>1):
-        return n*fact(int(n-1))
+        return int(n)*fact(int(n-1))
     else:
         return int(1)
 n=int(input())
 a=input().split(" ")
-y,z=[]
+b=[0]
+h=1
+mod=1000000007
+for s in a:
+    b.append(int(s))
+y=[]
+z=[]
 q=int(input())
 for i in range(0,q):
-    y[i],z[i]=input().split(" ")
+    ww=input().split(" ")
+    y.append(int(ww[0]))
+    z.append(int(ww[1]))
+for i in range(0,q):
+    l=int(y[i])
+    r=int(z[i])
+    for j in range(l,r+1):
+        h*=fact(int(b[j]))
+    h=h%mod
+    h=h**(r-l)
+    print(h)
+
